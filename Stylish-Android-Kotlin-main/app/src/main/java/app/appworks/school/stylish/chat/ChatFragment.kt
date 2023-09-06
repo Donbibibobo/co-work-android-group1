8d7041dddc1d9eaa710fdef614132ebf4ed3c9e8
Dondon <twterry10@yahoo.com.tw>
2023-09-06 09:36:05 +0800

    Don merge

--- Stylish-Android-Kotlin-main/app/src/main/java/app/appworks/school/stylish/chat/ChatFragment.kt
+++ Stylish-Android-Kotlin-main/app/src/main/java/app/appworks/school/stylish/chat/ChatFragment.kt
@@ -1,22 +1,45 @@
 package app.appworks.school.stylish.chat
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import app.appworks.school.stylish.MainActivity
 import app.appworks.school.stylish.databinding.FragmentChatBinding
+import app.appworks.school.stylish.util.RealPathUtil
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
+import java.security.cert.CertPath
 
 class ChatFragment : Fragment() {
 
     private lateinit var viewModel: ChatViewModel
+    private lateinit var path: String
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        return inflater.inflate(R.layout.fragment_chat_gpt, container, false)
         val binding = FragmentChatBinding.inflate(inflater)
+
+        viewModel = ViewModelProvider(this)[ChatViewModel::class.java]
 
         val input = listOf(binding.gptInputText.text.toString())
         val adapter = ChatAdapter()
@@ -33,11 +56,74 @@ class ChatFragment : Fragment() {
 
         binding.sendButton.setOnClickListener {
             adapter.submitList(chatList)
+
+            // POST sent
         }
+
+        binding.imageButton.setOnClickListener{
+            // select Image
+            if (ContextCompat.checkSelfPermission(this.requireContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent()
+                intent.setType("image/*")
+                intent.setAction(Intent.ACTION_GET_CONTENT)
+                startActivityForResult(intent, 10)
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),1)
+            }
+        }
+
+
+        viewModel.bitmap.observe(viewLifecycleOwner, Observer {
+            binding.testImage.setImageBitmap(it)
+        })
+
+        binding.sendButton2.setOnClickListener{
+            // API vuttom
+            viewModel.addImage()
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
         return binding.root
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 10 && resultCode == Activity.RESULT_OK) {
+            val uri = data?.data
+            val context: Context = requireActivity()
+            path = RealPathUtil.getRealPath(context, uri!!).toString()
+            Log.i("ttrryy", "path: $path")
+
+            viewModel.bitmap.value = BitmapFactory.decodeFile(path)
+            Log.i("ttrryy", "bitmap: $viewModel.bitmap.valu")
+
+            viewModel.path = path
+
+        }
+    }
+
+
+
 }
--- Stylish-Android-Kotlin-main/app/src/main/java/app/appworks/school/stylish/chat/ChatViewModel.kt
+++ Stylish-Android-Kotlin-main/app/src/main/java/app/appworks/school/stylish/chat/ChatViewModel.kt
@@ -1,7 +1,38 @@
 package app.appworks.school.stylish.chat
 
+import android.graphics.Bitmap
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.appworks.school.stylish.data.UserTrackingRequestBodyString
+import app.appworks.school.stylish.network.UserStylishApi
+import app.appworks.school.stylish.util.ABtest
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 class ChatViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var bitmap =  MutableLiveData<Bitmap>()
+
+    var path: String = ""
+
+    fun addImage(){
+        val file = File(path)
+        val requestFile = file.asRequestBody("multipart/form-data".toMediaTypeOrNull())
+        val body = MultipartBody.Part.createFormData("image", file.name, requestFile)
+
+        viewModelScope.launch {
+            val response = UserStylishApi.retrofitService.userTrackingChatImage(body)
+
+            Log.i("userTracking", "[login]: ${response.chatResponse}")
+            Log.i("userTracking", "[login]: ${response.errorMessage}")
+            Log.i("userTracking", "[login_content]: $response")
+        }
+    }
+
 }
